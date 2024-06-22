123b38fdf19b6de6917ff3371fae3bb4d7925c9a
Benjamin Lu <benceruleanlu@proton.me>
2024-06-22 10:16:18 -0400

    fix: hotfix footer to mobile

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -8,14 +8,14 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div className='flex flex-row justify-between mb-8 w-10/12 mx-auto border-t'>
+    <div className='flex flex-row justify-between mb-8 w-10/12 mx-auto border-t items-center'>
       <div className='flex flex-col'>
         <a href="https://www.peelschools.org/" target='_blank'>
           <Image
             src={peelImage}
             alt="Logo of Peel District School Board"
-            width={400}
-            className='opacity-60 m-8 ml-6'
+            width={250}
+            className='opacity-60 m-8 ml-6 md:w-[400px]'
           />
         </a>
         <div className='flex flex-row ml-6 space-x-12 mt-4'>
@@ -25,7 +25,7 @@ const Footer = () => {
           <a href="https://www.youtube.com/user/PeelSchools" target='_blank'><Image src={youtubeLogo} alt="YouTube Logo" width={24} height={24} className='opacity-60' /></a>
         </div>
       </div>
-      <div className='w-50 h-50 flex flex-row max-h-48'>
+      <div className='w-50 h-50 flex-row max-h-48 hidden custom-footer:flex'>
         <div className='flex flex-col mr-24 mt-8 space-y-4'>
           <p>Articles</p>
           <Link href="/portrait-of-a-learner-engaged-in-iselp" className='text-slate-600 transition duration-300 ease-in-out transform hover:scale-105'>Portrait</Link>
