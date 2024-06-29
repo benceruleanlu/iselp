f4d1b080ba3126dfaec83e5aa77cc8a1cbd01349
Dukesil <90260474+Dukesil@users.noreply.github.com>
2024-06-29 17:15:05 -0400

    patch for broken text updating

--- app/components/searchbar.tsx
+++ app/components/searchbar.tsx
@@ -2,28 +2,31 @@
 
 import Image from 'next/image';
 import searchIcon from '/public/searchIcon.png';
-import Link from 'next/link';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+
 const SearchBar = () => {
 
-    function evaluatePlaceholder() {
-        if (sessionStorage.getItem("currentQuery")) {
-            return sessionStorage.getItem("currentQuery");
+    useEffect(() => {
+        const inputField = document.getElementById("searchBarText") as HTMLInputElement;
+        console.error(sessionStorage.getItem("currentQuery"));
+        if (typeof sessionStorage.getItem("currentQuery") !== 'undefined') { // build in asynchronous element, html seems to loads before the update
+            inputField.placeholder = sessionStorage.getItem("currentQuery")!;
         }
         else {
-            return "Search";
+            inputField.placeholder = "Search";
         }
-    }
-
+    })
+    
     return (
     <div className = 'bg-slate-50 text-slate-700 rounded-xl p-4 min-w-[60vw] lg:min-w-[42vw]'>
         <form className = "flex flex-row justify-between" method = "get" action = "/search-landing-page">
+
             <input className = 'text-left focus:outline-none fill bg-slate-50 grow'
             type="text" 
-            id="searchBar" 
+            id="searchBarText" 
             name ="q"
             onChange={(event) => sessionStorage.setItem("currentQuery", event.target.value)}
-            placeholder = {evaluatePlaceholder()}/>
+            placeholder = "Search"/>
 
             <button type="submit">
             <Image 
