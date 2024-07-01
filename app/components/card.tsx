da556c876006410823da0a8006fb359fa762780b
Benjamin Lu <benceruleanlu@proton.me>
2024-06-30 23:02:57 -0400

    fix: card adjustment issue

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -14,8 +14,8 @@ const Card: React.FC<CardProps> = ({ title, imageSrc, subtitle, linkUrl }) => {
     <div className="max-w-lg rounded-lg shadow-lg mx-2 my-4 lg:m-6 w-[90vw] items-center border transition duration-300 ease-in-out hover:shadow-xl hover:lg:scale-105">
         <Link href={linkUrl} passHref>
         <Image src={imageSrc} alt={title} width={999} height={999} className='max-h-[200px] md:max-h-[300px] overflow-hidden object-cover rounded-t-lg'/>
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{title}</div>
+        <div className="px-6 py-4 shrink">
+          <div className="font-bold text-xl mb-2 tracking-tight">{title}</div>
           <p className="text-gray-700 text-base	min-h-[3rem] text-wrap">
             {subtitle}
           </p>
