e31e8122a4d82f17d17623a23a030475fa7f17fb
Benjamin Lu <benceruleanlu@proton.me>
2024-07-03 23:35:18 -0400

    chore: very opinionated style changes

--- app/search-landing-page/page.tsx
+++ app/search-landing-page/page.tsx
@@ -7,11 +7,11 @@ const Page = () => {
     <>
       <NavBar />
       <main>
-        <div className="flex flex-col items-start space-y-8 bg-[#0066CC] px-[5vw] pb-[7vh] pt-[7vh] text-left text-slate-50 md:space-y-16 md:px-[2vw] md:pt-[10vh]">
+        <div className="flex flex-col items-start space-y-8 bg-[#0066CC] p-8 text-left text-slate-50 md:py-12">
           <p className="max-w-[90vw] text-4xl font-bold md:max-w-[70vw] md:text-5xl lg:text-7xl">
             What would you like to learn today?
           </p>
-          <div className="min-w-[60vw] rounded-xl bg-slate-50 p-4 text-slate-700 lg:min-w-[42vw]">
+          <div className="min-w-[60vw] rounded-xl lg:p-2 bg-slate-50 text-slate-700 lg:min-w-[42vw]">
             <SearchBar />
           </div>
         </div>
