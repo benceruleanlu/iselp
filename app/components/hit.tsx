036acbdd169707dcd1370fa7dd2c2346dab2f3cf
Benjamin Lu <benceruleanlu@proton.me>
2024-07-14 16:27:33 -0400

    fix: tabs to spaces

--- app/components/hit.tsx
+++ app/components/hit.tsx
@@ -2,18 +2,17 @@ import { Highlight, Snippet } from "react-instantsearch";
 import { getPropertyByPath } from 'instantsearch.js/es/lib/utils';
 
 export const Hit: React.FC<{ hit: any }> = ({ hit }) => {
-	return (
-		<article>
-			<img src={hit.poster_path} />
-			<div className="hit-article_title">
-				<Highlight attribute="article_title" hit={hit} />
-			</div>
-			<div className="hit-heading">
-				<Highlight attribute="heading" hit={hit} />
-			</div>
-			<div className="hit-text">
-				<Snippet attribute="text" hit={hit} />
-			</div>
-		</article>
-	);
+  return (
+    <article>
+      <div className="hit-article_title">
+        <Highlight attribute="article_title" hit={hit} />
+      </div>
+      <div className="hit-heading">
+        <Highlight attribute="heading" hit={hit} />
+      </div>
+      <div className="hit-text">
+        <Snippet attribute="text" hit={hit} />
+      </div>
+    </article>
+  );
 };
